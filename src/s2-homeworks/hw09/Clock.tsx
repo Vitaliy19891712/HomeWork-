bc55fdf837c7b09fd165297666f462024fc48896
Vitaliy19891712 <stopkaiko@mail.ru>
2023-02-27 21:41:21 +0300

    commit

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -35,8 +35,8 @@ function Clock() {
     // пишут студенты // спрятать дату если мышка не наведена
   };
 
-  const stringTime = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}` || <br />; // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-  const stringDate = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}` || <br />; // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+  const stringTime = date.toLocaleTimeString() || <br />; // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+  const stringDate = date.toLocaleDateString() || <br />; // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
   // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
   let day;
@@ -65,7 +65,8 @@ function Clock() {
     default:
       break;
   }
-  const stringDay = day || <br />; // пишут студенты
+    const stringDay = day || <br />; // пишут студенты
+    
   let month;
   switch (date.getMonth()) {
     case 1:
